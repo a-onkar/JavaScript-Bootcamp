5c62c58b1fb5957acba5890c369fee484c0bb04d
a-onkar <mailanandteerth@gmail.com>
2020-02-25 15:52:23 +0000

    'DT & Operators'

--- DataTypes.js
+++ DataTypes.js
@@ -40,22 +40,82 @@ const emp_id = 8;
 /* *******************************************************************************************************************************
      Objects, Array, Function: Here we will just see the basic syntax. In depth description will be added in different JS file.
 ****************************************************************************************************************************** */
+
 //Objects
+
+console.log("Example of Object declaration and accesing variables of an object")
 
 let udobj = {} // Undefined object
 
 let user = {
     name: "Anand",
-    age:33
+    age:30
 };
 
 console.log(user);
-console.log(user.name);
-user.age = 35;
+console.log('User name before updating: '+user.name);
 
-console.log(user.age);
+user.age = 36;   // Updating variable value using dot
+user['name'] = "Supriya";    // Updating variable value using bracket notations
+
+console.log('User name after updating: '+user.name);
 
 
+
+//Arrays: List of objects
+console.log("Example of Array declaration and accesing variables of an object");
+
+let emptyArray = []; // Blank Array or Empty array
+
+let sampleArray = ['el0', 'el1', 'el2', 'el3'];
+
+console.log(sampleArray); // Priniting the complete Array
+console.log(sampleArray[3]); // Printing specific element in an array
+
+console.log('Length of Array: '+sampleArray.length); // Printing the length of array
+
+sampleArray[4] = 'el4';  // Adding new element to Array
+
+console.log(sampleArray); // Priniting the updated Array
+console.log('Length of updated Array: '+sampleArray.length); // Printing the length of array
+
+
+// Functions
+
+function printData(){
+    console.log("This is a basic function, that is printing this statement")
+};
+
+printData();
+
+
+function greet (name, age){
+    console.log("Hello " + name + ', your age is '+age)
+};
+
+greet('Anand', '33'); // Calling the function with all variables
+greet('Supriya'); // Calling funcion with only one variable 
+greet();
+
+
+// Returning from a function 
+
+function addNumber(num1, num2){
+    return num1+num2;
+}
+
+let sum = addNumber(40,50);
+console.log(sum)
+console.log(addNumber(10,20))
+
+
+function sqrRoot(num){
+    let z = num * num;
+    return z;
+}
+
+console.log(sqrRoot(5))
+console.log(sqrRoot(25))
 
 
 
--- Operators.js
+++ Operators.js
@@ -2,31 +2,55 @@
 
 /****************** Arithematic Operators ******************/
 
-let a = 7, b = 21;
-let c; 
+let a = 7, b = 3;
 
-c= a + b;
-console.log('Addition: '+c);
-
-c = a % b;
-console.log('Modulus operation result: '+c);
-
-c = b / a;
-console.log('Division: '+c)
-
-a=5, b=2;
-c=a**b;  // a rasie to the power b
-console.log('Raise to the power: '+c)
+console.log(a+b);
+console.log(a-b);
+console.log(a%b);
+console.log(a/b)
+console.log(a**b) // a rasie to the power b
 
 
 /****************** Post-increement & Pre-Increement ******************/
 
-console.log('Post-increement & Pre-Increement');
+console.log('************Post-increement & Pre-Increement****************');
 
+// Example 1:
+console.log('*************Pre & Post increement example 1************');
+
+let p=10;
+
+// Post-increement
+console.log('Original value of p before increement: '+p)
+console.log('Value of p at post-increement (p++): '+ p++)  // Post-increement
+console.log('Value of p after increement: '+p)
+
+let q=10;
+// Pre-increement
+console.log('Original value of q before increement: '+q)
+console.log('Value of q at pre-increement (++q): '+ ++q)  // Pre-increement
+console.log('Value of q after increement: '+q)
+
+
+// Example 2:
+console.log('*************Pre & Post increement example 2************');
+let x = 1;
+let y = x ++; // First the value of x (i.e. 1) will be assigned to y & then the increement will take place.
+
+console.log(x) //2
+console.log(y) //1
+
+let x1=1;
+let y1 = ++x1;
+
+console.log(x1); //2
+console.log(y1); //2
+
+
+// Example 3:
+console.log('*************Pre & Post increement example 3************');
 let i=10;
-
 console.log('Original value of i: '+i)
-
 i++; // Post-increement
 console.log('Post-increement (i++): '+ i);
 
@@ -39,28 +63,16 @@ console.log("Pre-increement (++j): "+ j);
 
 let m=10;
 
-let x = 10 * m++; // 10 * 10 = 100
-
+x = 10 * m++; // 10 * 10 = 100
+console.log("Value of m after post-increement: "+m);
 console.log("Multiplying the post-increement (10 * m++): "+x);
 console.log("Value of m after post-increement: "+m);
 
 n = 10;
-let y = 10 * ++n; // 10 * 11 = 110
+y = 10 * ++n; // 10 * 11 = 110
 console.log("Multiplying the pre-increement (10 * ++n): "+y);
 console.log("Value of n after pre-increement: "+n);
 
-
-
-m=15, n= 15;
-
-x = 10* m--; // 10 * 15
-y = 10 * --n; // 10 * 14
-
-console.log("Multiplying the post-decreement (10 * m--): "+x);
-console.log("Value of m after post-decreement: "+m);
-
-console.log("Multiplying the pre-decreement (10 * --n): "+y);
-console.log("Value of n after pre-decreement: "+n);
 
 
 
